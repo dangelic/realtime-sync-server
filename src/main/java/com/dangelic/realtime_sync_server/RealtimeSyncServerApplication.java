955db2c85b410cff46d2638b25793c052835ef89
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-10-17 22:41:04 +0200

    (dev) include maven project

---
+++ src/main/java/com/dangelic/realtime_sync_server/RealtimeSyncServerApplication.java
@@ -0,0 +1,13 @@
+package com.dangelic.realtime_sync_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeSyncServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeSyncServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dangelic/realtime_sync_server/RealtimeSyncServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangelic.realtime_sync_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeSyncServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
