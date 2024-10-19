7e7d9942eb7f80bac06056e4e2f1c55737598375
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-10-19 19:45:19 +0200

    (dev) implement h2 test to client controller

--- src/main/java/com/dangelic/realtimesyncserver/controller/ClientController.java
+++ src/main/java/com/dangelic/realtimesyncserver/controller/ClientController.java
@@ -1,29 +1,27 @@
 package com.dangelic.realtimesyncserver.controller;
 
+import com.dangelic.realtimesyncserver.model.Client;
+import com.dangelic.realtimesyncserver.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    // Connect a new client
+    @Autowired
+    private ClientRepository clientRepository;
+
     @PostMapping("/connect")
-    public String connectClient(@RequestBody String clientData) {
+    public Client connectClient(@RequestBody Client clientData) {
         System.out.println("POST /api/clients/connect - Client connected");
-        return "{\"message\": \"Client connected\"}";
+        return clientRepository.save(clientData);
     }
 
-    // Disconnect an existing client by clientId
-    @PostMapping("/disconnect/{clientId}")
-    public String disconnectClient(@PathVariable String clientId) {
-        System.out.println("POST /api/clients/disconnect/" + clientId + " - Client disconnected");
-        return "{\"message\": \"Client disconnected\"}";
-    }
-
-    // Get the status of a specific client by clientId
-    @GetMapping("/{clientId}/status")
-    public String getClientStatus(@PathVariable String clientId) {
-        System.out.println("GET /api/clients/" + clientId + "/status - Client status retrieved");
-        return "{\"message\": \"Client status retrieved\"}";
+    @GetMapping("/")
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
     }
 }
