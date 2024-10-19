b884bcae1127f702ea7d0d500d1a0d61530a7b56
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-10-19 19:34:34 +0200

    (fix) app name mismatch

--- src/main/java/com/dangelic/realtimesyncserver/RealtimeSyncServerApplication.java
+++ src/main/java/com/dangelic/realtimesyncserver/RealtimeSyncServerApplication.java
@@ -3,7 +3,7 @@ package com.dangelic.realtimesyncserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.dangelic")
+@SpringBootApplication
 public class RealtimeSyncServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RealtimeSyncServerApplication.class, args);
